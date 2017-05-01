5d40055a1183a8e6c8e099deadf1ef6d8b3e5028
Sarah Jane Morris <apmorris83@gmail.com>
2017-05-01 10:44:57 +0100

    added bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
     render() {
         return (
-            <p>hello</p>
+            <h1>hello</h1>
         ); 
     }
 }
