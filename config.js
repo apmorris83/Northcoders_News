acbaf5225cf2fd4e8754f4d972f237f350c9aa8a
Sarah Jane Morris <apmorris83@gmail.com>
2017-05-01 17:52:51 +0100

    northcoders api hooked up

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  ROOT: 'https://northcoders-news-api.herokuapp.com/api',
+  // ROOT: 'https://northcoders-news-api.herokuapp.com/api',
+  ROOT: 'https://warm-tundra-62150.herokuapp.com/api',
   USERNAME: 'northcoder'
 };
