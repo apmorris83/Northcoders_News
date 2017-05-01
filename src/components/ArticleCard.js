6615c2c8b9bafa09f1f4a03c85f6fb93d2083d2a
Sarah Jane Morris <apmorris83@gmail.com>
2017-05-01 14:38:00 +0100

    displaying all article info

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-function ArticleCard (props) {
+function ArticleCard(props) {
     return (
         <div className="well">
-            <h1>{props.title}</h1>
+            <h3>{props.title}</h3>
+            <span>
+                <strong>{props.created_by}</strong>
+                {' | '}
+                <small>{props.belongs_to}</small>
+                {' | '}
+                <small>{props.comments} comments</small>
+            </span>
+            <button className="btn btn-success"><i className="glyphicon glyphicon-chevron-up"></i></button>
+                <span><b> {props.votes} </b></span>
+            <button className="btn btn-danger"><i className="glyphicon glyphicon-chevron-down"></i></button>
         </div>
     );
 }
