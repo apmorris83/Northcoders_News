c87114ef6159acaa5404b8db3ddbcdcb0e231131
Sarah Jane Morris <apmorris83@gmail.com>
2017-05-09 10:18:42 +0100

    updated images

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -7,7 +7,7 @@ export default class Jumbotron extends Component {
     render() {
         if (this.props.topic === undefined) {
                 return (
-                <div className="jumbotron vertical-center football">
+                <div className="jumbotron vertical-center all">
                     <div className="container">
                         <h2>{capitaliseHero(this.props.topic)} articles</h2>
                     </div>
