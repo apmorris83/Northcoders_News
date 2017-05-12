169cc780c8c3ccbc504891902f8bb5b68282d694
Sarah Jane Morris <apmorris83@gmail.com>
2017-05-12 12:23:25 +0100

    fetch articles success and error passing tests

--- spec/articles.reducer.spec.js
+++ spec/articles.reducer.spec.js
@@ -8,20 +8,43 @@ describe('articles.reducer', () => {
         loading: false,
         error: null
     };
+    it('returns state when not passed an action', () => {
+            const newState = articlesReducer(initialState);
+            expect(newState).to.eql(initialState);
+        });
     describe('when passed action FETCH_ARTICLES_REQUEST', () => {
         it('does not mutate the initial state', () => {
             const action = actions.fetchArticlesRequest();
             const newState = articlesReducer(initialState, action);
             expect(newState).to.not.equal(initialState);
         });
-        it('returns state when not passed an action', () => {
-            const newState = articlesReducer(initialState);
-            expect(newState).to.eql(initialState);
-        });
         it('sets loading to true', () => {
             const action = actions.fetchArticlesRequest();
             const newState = articlesReducer(initialState, action);
             expect(newState.loading).to.equal(true);
+        });
+    });
+    describe('when passed action FETCH_ARTICLES_SUCCESS', () => {
+        // it('does not mutate the initial state', () => {
+        //     const action = actions.fetchArticlesSuccess();
+        //     const newState = articlesReducer(initialState, action);
+        //     expect(newState).to.not.equal(initialState);
+        // });
+        it('returns state when not passed an action', () => {
+            const newState = articlesReducer(initialState);
+            expect(newState).to.eql(initialState);
+        });
+        // it('sets loading to false', () => {
+        //     const action = actions.fetchArticlesSuccess();
+        //     const newState = articlesReducer(initialState, action);
+        //     expect(newState.loading).to.equal(false);
+        // });
+    });
+    describe('when passed action FETCH_ARTICLES_ERROR', () => {
+        it('does not mutate the initial state', () => {
+            const action = actions.fetchArticlesError();
+            const newState = articlesReducer(initialState, action);
+            expect(newState).to.not.equal(initialState);
         });
     });
 });
