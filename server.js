3a7a1999a2aeae39fec365bc8699c6a31b3f3105
Sarah Jane Morris <apmorris83@gmail.com>
2017-05-16 11:21:10 +0100

    added route into server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
 app.listen(process.env.PORT || 8080, function () {
   console.log('server started');
 });
