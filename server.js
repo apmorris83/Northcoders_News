a48e40189c246d01b8d679998f707a2427c84941
Sarah Jane Morris <apmorris83@gmail.com>
2017-05-16 10:41:07 +0100

    amended procfile and server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.listen(process.env.PORT || 8080, function () {
+  console.log('server started');
 });
-
-app.listen(port);
-console.log('server started');
